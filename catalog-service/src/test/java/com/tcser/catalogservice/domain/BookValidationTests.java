1e859a89a13d15e28368f2f89b538cc0aa896145
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-05-28 15:49:22 -0300

    Unit tests with JUnit 5

---
+++ catalog-service/src/test/java/com/tcser/catalogservice/domain/BookValidationTests.java
@@ -0,0 +1,38 @@
+package com.tcser.catalogservice.domain;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class BookValidationTests {
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds() {
+        var book = new Book("1234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations, is(empty()));
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails() {
+        var book = new Book("a234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations, hasSize(1));
+        assertThat(violations.iterator().next().getMessage(), equalTo("The ISBN format must be valid."));
+    }
+}
